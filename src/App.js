e7e2a4b485cc44d35586992d7cceec8ea3dce85c
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-11-29 16:06:45 -0300

    feature: Create minhas-series Project and files bootstrap/reactStrap CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from "./components/Header"
+import axios, { AUTH_TOKEN } from 'axios'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+
+const Generos = () => {
+  return <h1>Generos</h1>
+}
+
+axios.defaults.baseURL = 'https://localhost:3002/';
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get("/api").then(res => {
+      setData(res.data)
+    })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <script type="text/javascript" src="https://www.turnjs.com/lib/turn.min.js "></script>
+        <Route path="/" exact component={Home} />
+        <Route path="/generos" component={Generos} />
+        <pre>{JSON.stringify(data)}</pre>
+      </div>
+    </Router>
   );
 }
 
